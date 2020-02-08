0d06d7fc9bf544933a8c9ec13e1c2cde7bbda4b4
jhhest <janvanhest@outlook.com>
2020-02-08 12:33:41 +0100

    Met all conditions till 1.6

--- messages-api.js
+++ messages-api.js
@@ -7,29 +7,54 @@
 
 // Add a single endpoint to the app responds to POST requests to the /messages URI.
 
-
 const cors = require("cors");
 const Express = require("express");
 const bodyParser = require("body-parser");
-const logMiddleware = require("./middleWareFunctions/logMiddleware");
+const {
+  logMiddleware,
+  checkNumberOfViews
+} = require("./middleWareFunctions/logMiddleware");
 
 const corsMiddleware = cors();
 
 const app = new Express();
 app.use(corsMiddleware, logMiddleware, bodyParser.json());
-app.get("/", (request, respond) =>
-  respond.send("<h1>Rest Api - Homework Assignment</h1>")
+
+app.get("/", (request, response) =>
+  response.send("<h1>Rest Api - Homework Assignment</h1>")
 );
+
+let numberOfViews = 0;
 app.post("/message", (request, response, next) => {
-  console.log("Log of text property of request.body: ", request.body)
-  response.send(request.body)
-})
+  console.log("Log of text property of request.body: ", request.body);
+  numberOfViews++;
+  console.log("This is your ", numberOfViews, " view.");
+
+  if (numberOfViews > 5) {
+    response.sendStatus(429).end();
+  } else {
+    if (!response.headerSent) {
+      const textPropsArr = Object.values(request.body);
+      let objectMeetRequirements = true;
+
+      for (let i = 0; i < textPropsArr.length; i++) {
+        if (textPropsArr[i] === "") {
+          objectMeetRequirements = false;
+        }
+      }
+
+      !objectMeetRequirements && response.sendStatus(500).end();
+
+      objectMeetRequirements && response.send(request.body);
+    }
+  }
+});
 
 const port = 3000;
 app.listen(port, () =>
   console.log(`
-/**
- * Server listens to port: ${port}
- **/
-`)
+  /**
+   * Server listens to port: ${port}
+   **/
+  `)
 );
--- middleWareFunctions/logMiddleware.js
+++ middleWareFunctions/logMiddleware.js
@@ -1,13 +1,27 @@
-const logMiddleware = (request, respond, next) => {
+const bodyParser = (require = "body-parser");
+const logMiddleware = (response, request, next) => {
   console.log(
-`
+    `
 /**
  * MiddleWare logging function.
  * New Request is initiated 
  **/
 `
-  );
+  )
   next();
 };
 
-module.exports = logMiddleware;
+let numberOfViews = 0;
+
+// const checkNumberOfViews = (request, response, next) => {
+//   numberOfViews++;
+//   console.log("This is your ", numberOfViews, " view.");
+//   if (numberOfViews > 5) {
+//     response.status(500).json({
+//       message: `You sended to many requests to endpoint`,
+//       givenInputByClient: request.body
+//     });
+//   }
+// };
+
+module.exports = { logMiddleware };
